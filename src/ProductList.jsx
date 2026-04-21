fe09b9172f294043fd40a8a9f4525815452bf269
holczin <holczin@freemail.hu>
2026-04-21 13:03:30 +0200

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
+    const cart = useSelector(state => state.cart.items); 
+
+    useEffect(() => {
+        const addedItems = {};
+        cart.forEach(item => {
+        addedItems[item.name] = true;
+        });
+        setAddedToCart(addedItems);
+    }, [cart]);
 
     const plantsArray = [
         {
@@ -218,7 +233,7 @@ function ProductList({ onHomeClick }) {
         padding: '15px',
         display: 'flex',
         justifyContent: 'space-between',
-        alignIems: 'center',
+        alignItems: 'center',
         fontSize: '20px',
     }
     const styleObjUl = {
@@ -232,6 +247,8 @@ function ProductList({ onHomeClick }) {
         fontSize: '30px',
         textDecoration: 'none',
     }
+
+    const totalItemsInCart = cart.reduce((acc, item) => acc + item.quantity, 0);
 
     const handleHomeClick = (e) => {
         e.preventDefault();
@@ -252,6 +269,15 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product)); 
+        setAddedToCart((prevState) => ({ 
+            ...prevState, 
+            [product.name]: true, 
+        }));
+    };
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -259,7 +285,7 @@ function ProductList({ onHomeClick }) {
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                         <a href="/" onClick={(e) => handleHomeClick(e)}>
-                            <div>
+                            <div style={{ marginLeft: '15px' }}>
                                 <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
                             </div>
@@ -268,13 +294,58 @@ function ProductList({ onHomeClick }) {
 
                 </div>
                 <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+                <a 
+                href="#" 
+                onClick={(e) => handlePlantsClick(e)} 
+                style={{ 
+                    ...styleA, 
+                    position: 'absolute', 
+                    left: '50%', 
+                    transform: 'translateX(-50%)' 
+                }}
+                >
+                Plants
+                </a>
+                </div>
+                    <div> 
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className='cart'>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
+                                <span className="cart_quantity_count">{totalItemsInCart}</span>
+                            </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
+                    {plantsArray.map((category, index) => ( // Loop through each category in plantsArray
+                    <div key={index}> 
+                        <h1 className="category-title">{category.category}</h1>
+                        <div className="product-list"> 
+                        {category.plants.map((plant, plantIndex) => ( 
+                            <div className="product-card" key={plantIndex}> 
+                            <img 
+                                className="product-image" 
+                                src={plant.image} 
+                                alt={plant.name} 
+                            />
+                            <div className="product-title">{plant.name}</div> 
+                            <div className="product-description">{plant.description}</div> 
+                            <div className="product-cost">${plant.cost}</div> 
+                            <button
+                                className={`product-button ${addedToCart[plant.name] ? 'added-to-cart' : ''}`}
+                                onClick={() => handleAddToCart(plant)}
+                                disabled={!!addedToCart[plant.name]}
+                                >
+                                {addedToCart[plant.name] ? 'Added to Cart' : 'Add to Cart'}
+                            </button>
+                            </div>
+                        ))}
+                        </div>
+                    </div>
+                    ))}
 
                 </div>
             ) : (
